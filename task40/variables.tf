47848b19aeeaa68c3b0b31a35eb2736b853f0d2f
Hide <cxl03157@gmail.ocm>
2026-03-03 23:24:24 +0900

    指摘事項を修正しました

--- task40/variables.tf
+++ task40/variables.tf
@@ -15,7 +15,7 @@ variable "vpc_cidr" {
 
 variable "public_subnet_cidrs" {
   type    = list(string)
-  default = ["10.0.1.0/24", "10.0.20.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
