26f89a2a43d2fdfb8eb2c2af9d1dbfe247f64321
Hide <cxl03157@gmail.ocm>
2026-03-07 21:45:50 +0900

    41 GitHubActionsの解説_CD環境構築の課題提出

---
+++ task41/tf/backend.tf
@@ -0,0 +1,9 @@
+# backendの定義、lock追加
+terraform {
+  backend "s3" {
+    bucket = "tf-aws-study"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
