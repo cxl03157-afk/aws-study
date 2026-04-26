5eea4b5b26d5d16b41ab382ae95eb1b504716dfe
Hide <cxl03157@gmail.ocm>
2026-04-26 21:51:23 +0900

    security.tf　書式修正

--- projects/ansible-app-deploy/tf/security.tf
+++ projects/ansible-app-deploy/tf/security.tf
@@ -52,13 +52,13 @@ resource "aws_security_group" "ec2_sg" {
   }
 
 
-# PCからのアクセス
-#  ingress {
-#    from_port   = 22
-#    to_port     = 22
-#    protocol    = "tcp"
-#    cidr_blocks = [var.ec2_ssh_cidr]
-#  }
+  # PCからのアクセス
+  #  ingress {
+  #    from_port   = 22
+  #    to_port     = 22
+  #    protocol    = "tcp"
+  #    cidr_blocks = [var.ec2_ssh_cidr]
+  #  }
 
   egress {
     from_port   = 0
